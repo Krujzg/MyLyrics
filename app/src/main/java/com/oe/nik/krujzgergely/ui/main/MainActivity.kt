6161405f9b0a621107dbf93d756d4f266a1cf6c1
Krujz Gergely <krujzg97@gmail.com>
2020-07-13 13:57:53 +0200

    Starting Animation Added

--- app/src/main/java/com/oe/nik/krujzgergely/ui/main/MainActivity.kt
+++ app/src/main/java/com/oe/nik/krujzgergely/ui/main/MainActivity.kt
@@ -1,37 +1,56 @@
 package com.oe.nik.krujzgergely.ui.main
 
+import android.app.ActivityOptions
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.TaskInfo
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.oe.nik.krujzgergely.R
 import com.oe.nik.krujzgergely.ui.lyrics.LyricsActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
-        val startButton = findViewById<Button>(R.id.btn_Start)
-        listenerForStartButtonClickToStartLyricsActivity(startButton)
-
-        startButton.setOnClickListener{startActivity( Intent(this, LyricsActivity::class.java)) }
+        doAnimatonAutomatically()
 
         createNotificationChannel(getString(R.string.mylyrics_notification_channel_id),getString(R.string.mylyrics_notification_channel_name))
     }
 
-    private fun listenerForStartButtonClickToStartLyricsActivity(startButton : Button)
+    private fun doAnimatonAutomatically()
     {
-        startButton.setOnClickListener{startActivity( Intent(this, LyricsActivity::class.java)) }
+        findViewById<MotionLayout>(R.id.motion_base).transitionToEnd()
+        delayStartActivityTillAnimationEnds()
+    }
+
+    private fun delayStartActivityTillAnimationEnds()
+    {
+        val runnableFunction = Runnable { startLyricsActivity() }
+        val delayHandler = Handler()
+
+        delayHandler.postDelayed(runnableFunction,3000)
+    }
+
+    private fun startLyricsActivity()
+    {
+        startActivity(Intent(this, LyricsActivity::class.java))
     }
 
     private fun createNotificationChannel(channelId: String, channelName: String)
